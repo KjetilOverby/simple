de86becb425fed9d5af3bbc69c73714b8493eb3f
KjetilOverby <kjeoever@msn.com>
2024-11-14 19:31:31 +0100

    Merge branch 'testbranch'jj

--- simple.js
+++ simple.js
@@ -1,12 +1,1 @@
-console.log("New Change from GitHub");
-console.log("Hello world2!");
-
-console.log("new Change VSCode");
-console.log("testbranch");
-
-console.log("The third branch");
-
-console.log("Made before merging the testbranch");
-
-const new_feature = "The new feature";
-console.log(new_feature);
+console.log("main branch");
